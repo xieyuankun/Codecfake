41bd38df473b4769c9e285146a7dea5cc511f325
Sonic Blaster <51701794+xieyuankun@users.noreply.github.com>
2024-05-12 21:43:24 +0800

    update

--- model.py
+++ model.py
@@ -547,8 +547,110 @@ class W2VAASIST(nn.Module):
 
         return last_hidden,output
 
+
+
+
+
+class MaxFeatureMap2D(nn.Module):
+    """ Max feature map (along 2D)
+    MaxFeatureMap2D(max_dim=1)
+    l_conv2d = MaxFeatureMap2D(1)
+    data_in = torch.rand([1, 4, 5, 5])
+    data_out = l_conv2d(data_in)
+    Input:
+    ------
+    data_in: tensor of shape (batch, channel, ...)
+    Output:
+    -------
+    data_out: tensor of shape (batch, channel//2, ...)
+    Note
+    ----
+    By default, Max-feature-map is on channel dimension,
+    and maxout is used on (channel ...)
+    """
+
+    def __init__(self, max_dim=1):
+        super(MaxFeatureMap2D, self).__init__()
+        self.max_dim = max_dim
+
+    def forward(self, inputs):
+        # suppose inputs (batchsize, channel, length, dim)
+
+        shape = list(inputs.size())
+
+        if self.max_dim >= len(shape):
+            print("MaxFeatureMap: maximize on %d dim" % (self.max_dim))
+            print("But input has %d dimensions" % (len(shape)))
+            sys.exit(1)
+        if shape[self.max_dim] // 2 * 2 != shape[self.max_dim]:
+            print("MaxFeatureMap: maximize on %d dim" % (self.max_dim))
+            print("But this dimension has an odd number of data")
+            sys.exit(1)
+        shape[self.max_dim] = shape[self.max_dim] // 2
+        shape.insert(self.max_dim, 2)
+
+        # view to (batchsize, 2, channel//2, ...)
+        # maximize on the 2nd dim
+        m, i = inputs.view(*shape).max(self.max_dim)
+        return m
+
         
-        
+class LCNN(nn.Module):
+    def __init__(self, num_nodes, enc_dim, nclasses=2):
+        super(LCNN, self).__init__()
+        self.num_nodes = num_nodes
+        self.enc_dim = enc_dim
+        self.nclasses = nclasses
+        self.conv1 = nn.Sequential(nn.Conv2d(1, 64, (5, 5), 1, padding=(2, 2)),
+                                   MaxFeatureMap2D(),
+                                   nn.MaxPool2d((2, 2), (2, 2)))
+        self.conv2 = nn.Sequential(nn.Conv2d(32, 64, (1, 1), 1, padding=(0, 0)),
+                                   MaxFeatureMap2D(),
+                                   nn.BatchNorm2d(32, affine=False))
+        self.conv3 = nn.Sequential(nn.Conv2d(32, 96, (3, 3), 1, padding=(1, 1)),
+                                   MaxFeatureMap2D(),
+                                   nn.MaxPool2d((2, 2), (2, 2)),
+                                   nn.BatchNorm2d(48, affine=False))
+        self.conv4 = nn.Sequential(nn.Conv2d(48, 96, (1, 1), 1, padding=(0, 0)),
+                                   MaxFeatureMap2D(),
+                                   nn.BatchNorm2d(48, affine=False))
+        self.conv5 = nn.Sequential(nn.Conv2d(48, 128, (3, 3), 1, padding=(1, 1)),
+                                   MaxFeatureMap2D(),
+                                   nn.MaxPool2d((2, 2), (2, 2)))
+        self.conv6 = nn.Sequential(nn.Conv2d(64, 128, (1, 1), 1, padding=(0, 0)),
+                                   MaxFeatureMap2D(),
+                                   nn.BatchNorm2d(64, affine=False))
+        self.conv7 = nn.Sequential(nn.Conv2d(64, 64, (3, 3), 1, padding=(1, 1)),
+                                   MaxFeatureMap2D(),
+                                   nn.BatchNorm2d(32, affine=False))
+        self.conv8 = nn.Sequential(nn.Conv2d(32, 64, (1, 1), 1, padding=(0, 0)),
+                                   MaxFeatureMap2D(),
+                                   nn.BatchNorm2d(32, affine=False))
+        self.conv9 = nn.Sequential(nn.Conv2d(32, 64, (3, 3), 1, padding=[1, 1]),
+                                   MaxFeatureMap2D(),
+                                   nn.MaxPool2d((2, 2), (2, 2)))
+        self.out = nn.Sequential(nn.Dropout(0.7),
+                                 nn.Linear(24576, 160),
+                                 MaxFeatureMap2D(),
+                                 nn.Linear(80, self.enc_dim))
+        self.fc_mu = nn.Linear(enc_dim, nclasses) if nclasses >= 2 else nn.Linear(enc_dim, 1)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.conv6(x)
+        x = self.conv7(x)
+        x = self.conv8(x)
+        x = self.conv9(x)
+        feat = torch.flatten(x, 1)
+        print(feat.shape)
+        feat = self.out(feat)
+        out = self.fc_mu(feat)
+
+        return feat, out
         
 if __name__ == "__main__":
     os.environ["CUDA_VISIBLE_DEVICES"] = "7"
--- preprocess.py
+++ preprocess.py
@@ -53,26 +53,41 @@ for part_ in ["train", "dev"]:
     print("Done")
 
 
-for part_ in ["train", "dev"]:
-    asvspoof_raw = dataset.ASVspoof2019Raw("LA", "/data2/xyk/asv2019/LA",
-                                           "/data2/xyk/asv2019/LA/ASVspoof2019_LA_cm_protocols/", part=part_)
-    target_dir = os.path.join("/data2/xyk/asv2019/preprocess_xls-r-5", part_,
-                              "xls-r-5")
-    processor = Wav2Vec2FeatureExtractor.from_pretrained("facebook/wav2vec2-xls-r-300m")
-    model = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-xls-r-300m").cuda()
-    model.config.output_hidden_states = True
-    if not os.path.exists(target_dir):
-        os.makedirs(target_dir)
-    for idx in tqdm(range(len(asvspoof_raw))):
-        waveform, filename, tag, label = asvspoof_raw[idx]
-        waveform = pad_dataset(waveform, 64600)
-        input_values = processor(waveform, sampling_rate=16000,
-                                 return_tensors="pt").input_values.cuda()
-
-        with torch.no_grad():
-            wav2vec2 = model(input_values).hidden_states[5].cpu()
-        print(wav2vec2.shape)
-        torch.save(wav2vec2, os.path.join(target_dir, "%05d_%s_%s_%s.pt" % (idx, filename, tag, label)))
-    print("Done!")
-
-
+# for part_ in ["train", "dev"]:
+#     asvspoof_raw = dataset.ASVspoof2019Raw("LA", "/data2/xyk/asv2019/LA",
+#                                            "/data2/xyk/asv2019/LA/ASVspoof2019_LA_cm_protocols/", part=part_)
+#     target_dir = os.path.join("/data2/xyk/asv2019/preprocess_xls-r-5", part_,
+#                               "xls-r-5")
+#     processor = Wav2Vec2FeatureExtractor.from_pretrained("facebook/wav2vec2-xls-r-300m")
+#     model = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-xls-r-300m").cuda()
+#     model.config.output_hidden_states = True
+#     if not os.path.exists(target_dir):
+#         os.makedirs(target_dir)
+#     for idx in tqdm(range(len(asvspoof_raw))):
+#         waveform, filename, tag, label = asvspoof_raw[idx]
+#         waveform = pad_dataset(waveform, 64600)
+#         input_values = processor(waveform, sampling_rate=16000,
+#                                  return_tensors="pt").input_values.cuda()
+#
+#         with torch.no_grad():
+#             wav2vec2 = model(input_values).hidden_states[5].cpu()
+#         print(wav2vec2.shape)
+#         torch.save(wav2vec2, os.path.join(target_dir, "%05d_%s_%s_%s.pt" % (idx, filename, tag, label)))
+#     print("Done!")
+#
+# for part_ in ["train", "dev"]:
+#     asvspoof_raw = dataset.ASVspoof2019Raw("LA", "/data2/xyk/asv2019/LA",
+#                                            "/data2/xyk/asv2019/LA/ASVspoof2019_LA_cm_protocols/", part=part_)
+#     target_dir = os.path.join("/data2/xyk/asv2019/preprocess_mel", part_,
+#                               "mel")
+#     if not os.path.exists(target_dir):
+#         os.makedirs(target_dir)
+#     for idx in tqdm(range(len(asvspoof_raw))):
+#         waveform, filename, tag, label = asvspoof_raw[idx]
+#         waveform = pad_dataset(waveform)
+#         print(waveform.shape)
+#         mel = torchaudio.transforms.MelSpectrogram(n_mels=80, n_fft=512, hop_length=128, sample_rate=16000)(waveform)
+#         mel = mel.unsqueeze(dim=0)
+#         print(mel.shape)
+#         torch.save(mel, os.path.join(target_dir, "%05d_%s_%s_%s.pt" % (idx, filename, tag, label)))
+#     print("Done!")
